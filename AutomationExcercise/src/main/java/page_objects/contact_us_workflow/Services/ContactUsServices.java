7b23d1fbd5391662f2064fd3f02454c1a11dd6fc
sebastianmina0 <sebastianmina654@hotmail.com>
2026-08-28 21:16:19 -0500

    Documentation complte for Test Case 6 clasess

--- AutomationExcercise/src/main/java/page_objects/contact_us_workflow/Services/ContactUsServices.java
+++ AutomationExcercise/src/main/java/page_objects/contact_us_workflow/Services/ContactUsServices.java
@@ -12,38 +12,65 @@ public class ContactUsServices {
     private final WebDriver driver;
     private final ContactUsUI contactUsUI;
 
+    /**
+     * Constructor
+     * @param driver
+     */
     public ContactUsServices(WebDriver driver){
 
         this.driver = driver;
         this.contactUsUI = new ContactUsUI(driver);
     }
 
+    /**
+     * Method that enters name in textbox
+     * @param name
+     */
     public void enterName(String name){
 
         contactUsUI.nameTextBox().sendKeys(name);
     }
 
+    /**
+     * Method that enters email in textbox
+     * @param email
+     */
     public void enterEmail(String email){
 
         contactUsUI.emailTextBox().sendKeys(email);
     }
 
+    /**
+     * Method that enters subject in textbox
+     * @param subject
+     */
     public void enterSubject(String subject){
 
         contactUsUI.subjectTextBox().sendKeys(subject);
     }
 
+    /**
+     * Method that enters message in textbox
+     * @param message
+     */
     public void enterMessage(String message){
 
         contactUsUI.messageTextBox().sendKeys(message);
 
     }
 
+    /**
+     * Method that uploads input file
+     * @param filePath
+     */
     public void enterFile(String filePath){
 
         contactUsUI.fileButton().sendKeys(filePath);
     }
 
+    /**
+     * Method that clicks submit button
+     */
     public void clickSubmitButton(){
 
         AdHandlerUtility.safeClick(driver, contactUsUI.submitButton());
--- AutomationExcercise/src/main/java/page_objects/contact_us_workflow/UI/ContactUsUI.java
+++ AutomationExcercise/src/main/java/page_objects/contact_us_workflow/UI/ContactUsUI.java
@@ -8,58 +8,106 @@ public class ContactUsUI {
 
     private WebDriver driver;
     
+    /**
+     * Constructor
+     * @param driver
+     */
     public ContactUsUI(WebDriver driver){
         this.driver = driver;
     }
 
+    /**
+     * Get driver
+     * @return
+     */
     public WebDriver getDriver(){
         return driver;
     }
 
+    /**
+     * Set driver
+     * @param driver
+     */
     public void setDriver(WebDriver driver){
         this.driver = driver;
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement contactUsPage(){
 
         return driver.findElement(By.cssSelector("div[id='contact-page'"));
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement nameTextBox(){
 
         return driver.findElement(By.cssSelector("input[data-qa='name']"));
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement emailTextBox(){
 
         return driver.findElement(By.cssSelector("input[data-qa='email']"));
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement subjectTextBox(){
 
         return driver.findElement(By.cssSelector("input[data-qa='subject']"));
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement messageTextBox(){
 
         return driver.findElement(By.cssSelector("textarea[data-qa='message']"));
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement fileButton(){
 
         return driver.findElement(By.cssSelector("input[type='file']"));
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement submitButton(){
 
         return driver.findElement(By.cssSelector("input[data-qa='submit-button']"));
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement successText(){
 
         return driver.findElement(By.cssSelector(".status.alert.alert-success"));
     }
 
+    /**
+     * Return web element
+     * @return
+     */
     public WebElement homeGreenButton(){
 
         return driver.findElement(By.cssSelector(".btn.btn-success"));
